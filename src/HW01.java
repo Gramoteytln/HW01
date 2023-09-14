14ebf002f45eadee01faf345207cb84f5a9ef5da
anatolyrusetskiy <anatoli.rusetski@gmail.com>
2023-09-14 21:47:34 +0300

    My comment

---
+++ src/HW01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HW01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello World, what you name?");
+        String name = scanner.nextLine();
+
+
+        System.out.println("Welcome, " + name);
+
+    }
+}
